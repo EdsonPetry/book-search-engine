2e7fc5858c4b6eb469d1bb0af48613e3b2e021b7
Edson Petry <edsonlbpetry@gmail.com>
2023-05-18 15:11:18 -0400

    .

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,49 +1,66 @@
-// use this to decode a token and get the user's information out of it
-import decode from 'jwt-decode';
+import { setContext } from "apollo-link-context";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { createHttpLink } from "apollo-link-http";
+import decode from "jwt-decode";
 
-// create a new class to instantiate for a user
 class AuthService {
+  // create an Apollo Client instance
+  client = new ApolloClient({
+    link: createHttpLink({
+      uri: "/graphql", // Update the URI to your GraphQL API endpoint
+    }),
+    cache: new InMemoryCache(),
+  });
+
   // get user data
   getProfile() {
-    return decode(this.getToken());
+    const token = this.getToken();
+    if (token) {
+      return decode(token);
+    }
+    return null;
   }
 
-  // check if user's logged in
+  // check if user is logged in
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return !!token && !this.isTokenExpired(token);
   }
 
   // check if token is expired
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
+      return decoded.exp < Date.now() / 1000;
     } catch (err) {
-      return false;
+      return true;
     }
   }
 
   getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
   }
 
   logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
   }
+
+  // create an Apollo Link middleware for authentication
+  authLink = setContext((_, { headers }) => {
+    const token = this.getToken();
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
 }
 
 export default new AuthService();
